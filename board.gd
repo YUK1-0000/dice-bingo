790e8c622860d56e16b7061cbfb11bccd504ce85
YUK1 <tanaka543taro@gmail.com>
2024-06-17 15:30:20 +0900

    書き換えてみた

--- board.gd
+++ board.gd
@@ -13,8 +13,10 @@ func _ready() -> void:
 	grid.columns = length
 	
 	var slot: DiceSlot
+	
 	for y in length:
 		for x in length:
+			
 			slot = preload("res://dice_slot.tscn").instantiate()
 			slot.coords = Vector2(x, y)
 			grid.add_child(slot)
@@ -33,44 +35,48 @@ func is_full() -> bool:
 
 func get_bingo_slots() -> Array[Array]:
 	
-	var ret: Array[Array] = []
-	var tmp: Array[DiceSlot] = []
+	var slot_arrays: Array[Array] = []
+	
 	for i in length:
-		
-		# 垂直方向のビンゴの取得し、戻り値に追加
-		tmp = slots.filter(
+		for f in [
+			
+			# 垂直方向に並ぶスロットを示す関数
 			func(slot: DiceSlot) -> bool:
-				return slot.coords.x == i and slot.has_dice()
-		)
-		if tmp.size() == length:
-			ret.append(tmp)
-		
-		# 水平方向のビンゴを取得し、戻り値に追加
-		tmp = slots.filter(
+				return slot.coords.x == i,
+			
+			# 水平方向に並ぶスロットを示す関数
 			func(slot: DiceSlot) -> bool:
-				return slot.coords.y == i and slot.has_dice()
-		)
-		if tmp.size() == length:
-			ret.append(tmp)
+				return slot.coords.y == i
+		]:
+			
+			# 関数fを満たすスロットの配列を追加
+			slot_arrays.append(slots.filter(f))
 	
-	# 左上から右下方向のビンゴを取得し、戻り値に追加
-	tmp = slots.filter(
+	for f in [
+		
+		# 斜め（左上から右下）方向に並ぶスロットを示す関数
 		func(slot: DiceSlot) -> bool:
-			return slot.coords.x == slot.coords.y and slot.has_dice()
-	)
-	if tmp.size() == length:
-		ret.append(tmp)
-	
-	# 右上から左下方向のビンゴを取得し、戻り値に追加
-	tmp = slots.filter(
+			return slot.coords.x == slot.coords.y,
+		
+		# 斜め（右上から左下）方向に並ぶスロットを示す関数
 		func(slot: DiceSlot) -> bool:
-			return slot.coords.x == length - 1 - slot.coords.y and slot.has_dice()
-	)
-	if tmp.size() == length:
-		ret.append(tmp)
+			return slot.coords.x == length - 1 - slot.coords.y
+	]:
+		
+		# 関数fを満たすスロットの配列を追加
+		slot_arrays.append(slots.filter(f))
 	
+	# ダイスが全てのスロットにある配列のみ戻り値にする
+	var ret := slot_arrays.filter(
+		func(array: Array) -> bool:
+			return array.all(
+				func(slot: DiceSlot) -> bool:
+					return slot.has_dice()
+			)
+	)
 	print("bingo: ", str(ret.size()))
 	print("bingo slots: ", ret)
+	
 	return ret
 
 
